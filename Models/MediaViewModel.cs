3de13c32fdd21c0c4165e7460dae5c4982dd6012
Konrad Marciniak <konrad.marciniak@microsoft.wsei.edu.pl>
2025-01-22 20:18:27 +0100

    Restore back to InitialCreate

---
+++ Models/MediaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Shared.Enums;
+
+namespace biblioteka.Models;
+public class MediaViewModel
+{
+
+    [HiddenInput]
+    public int? Id { get; set; }
+
+    [Required(ErrorMessage = "Title is missing!")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "Author is missing!")]
+    public string Author { get; set; }
+
+    public Status? Status { get; set; }
+
+    public Category? Category { get; set; }
+
+}
+
